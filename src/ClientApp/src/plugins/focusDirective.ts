2de9e701be490e6430b0bdac9cfa51feb16d506d
John Campion <john@brightshore.com>
2021-01-12 21:58:12 -0500

    tweaked focus

--- src/ClientApp/src/plugins/focusDirective.ts
+++ src/ClientApp/src/plugins/focusDirective.ts
@@ -1,5 +1,5 @@
 // https://github.com/simplesmiler/vue-focus
-// undefined allows for not includig ="true" everywhere
+// undefined allows for not includig ="true"
 import { App } from 'vue'
 
 export default {
@@ -12,12 +12,13 @@ export default {
       },
 
       updated(el, binding) {
-        if (binding.modifiers.lazy) {
-          if (Boolean(binding.value) === Boolean(binding.oldValue)) {
-            return
-          }
+        // Had to default to only calling focus again if the value changed, otherwise constantly regrabbed the focus in vue 3
+        // see docs for vue 1 vs 2 change in focus, its the same issue
+        // if (binding.modifiers.lazy) {
+        if (Boolean(binding.value) === Boolean(binding.oldValue)) {
+          return
         }
-
+        //}
         if (binding.value || binding.value === undefined) el.focus()
         else el.blur()
       }
